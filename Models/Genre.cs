c552ccbf3d1434decd9f004c5bf43bb4b14f4585
Andrew Fok <andrewmaestro1991@gmail.com>
2018-04-25 05:24:05 +0300

    Client-server validation added

---
+++ Models/Genre.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Music.Models
+{
+    public class Genre
+    {
+        public byte Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+    }
+}
